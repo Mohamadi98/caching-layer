0c6aa0f611b60e602d94dd06c0e691afce47f680
Mohamadi98 <mohamadinaena23@gmail.com>
2025-05-18 03:06:55 +0300

    added migration file to create products table, added dummy data sql

--- db/migrations/20250517224028_create_products_table.sql
+++ db/migrations/20250517224028_create_products_table.sql
@@ -1,10 +1,12 @@
 -- migrate:up
-
 CREATE TABLE products (
-    id INTEGER SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     price FLOAT NOT NULL,
     name VARCHAR(100) NOT NULL,
     category VARCHAR(50) NOT NULL,
     available_items integer,
-    created_at TIMESTAMP DEFAULT now(),
-)
+    created_at TIMESTAMP DEFAULT now()
+);
+
+-- migrate:down
+DROP TABLE products;
