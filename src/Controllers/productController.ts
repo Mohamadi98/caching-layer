5d5325e51b18b61dc8e92ef413bd632c43975eaa
Mohamadi98 <mohamadinaena23@gmail.com>
2025-05-19 06:18:10 +0300

    added a caching layer to retrieving products by id

--- src/Controllers/productController.ts
+++ src/Controllers/productController.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { dbClient } from "../database/postgres";
+import { redisClient } from "../database/redis";
 
 export const getProducts: RequestHandler = async(_req, res) => {
     const results = await dbClient.query('SELECT * FROM products;');
@@ -9,8 +10,21 @@ export const getProducts: RequestHandler = async(_req, res) => {
 }
 
 export const getProductsById: RequestHandler = async(req, res) => {
-    const result = await dbClient.query('SELECT * FROM products WHERE id = $1', [req.params.id]);
+    const id = req.params.id;
+    let result;
+    let fromCache = false;
+    const cachedData = await redisClient.get(id);
+
+    if(cachedData) {
+        fromCache = true;
+        result = JSON.parse(cachedData);
+    } else {
+        const dbData = await dbClient.query('SELECT * FROM products WHERE id = $1', [id]);
+        result = dbData.rows[0];
+        await redisClient.set(req.params.id, JSON.stringify(result));
+    }
     res.status(200).send({
-        data: result.rows[0]
+        fromCache: fromCache,
+        data: result
     });
 }
